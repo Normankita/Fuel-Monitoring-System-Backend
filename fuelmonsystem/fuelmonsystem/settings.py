86924dbfe1783fbba31157a68c03a7914b1bfe66
Michael Haule <mikael.haule1@gmail.com>
2024-06-17 15:50:04 +0300

    update URL

--- fuelmonsystem/fuelmonsystem/settings.py
+++ fuelmonsystem/fuelmonsystem/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'monitor.middleware.PrintRequestMiddleware', 
 ]
 
 ROOT_URLCONF = 'fuelmonsystem.urls'
